6615b99bf1dcabc886a67cb8f695f858392ed81f
SofyaKayava <kayava30@gmail.com>
2023-04-24 10:33:51 +0200

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,37 @@
+
+function getRandomInteger (a, b) {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function createRandomIdFromRangeGenerator (a, b, cb) {
+  const previousValues = [];
+  return function () {
+    let currentValue = cb(a, b);
+    while (previousValues.includes(currentValue)) {
+      currentValue = cb(a, b);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+function sortRandomly(arr){
+  let j, temp;
+  for(let i = arr.length - 1; i > 0; i--){
+    j = Math.floor(Math.random() * (i + 1));
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
+
+export {getRandomInteger, getRandomArbitrary,createRandomIdFromRangeGenerator,sortRandomly};
+
